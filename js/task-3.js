bcfff5f0a26893e158d4dccecc4088b08cfb9047
IrinaKataytseva <151220079+IrinaKataytseva@users.noreply.github.com>
2024-02-21 22:40:50 +0200

    task 2 done

--- js/task-3.js
+++ js/task-3.js
@@ -15,3 +15,13 @@
 // console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 // console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
+function filterArray(numbers, value) {
+    for 
+
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
