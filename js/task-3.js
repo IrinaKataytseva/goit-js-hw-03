775e713cc621cc20a5c0cfa4664f565646a01c00
IrinaKataytseva <151220079+IrinaKataytseva@users.noreply.github.com>
2024-02-23 23:27:52 +0200

    task 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,14 @@
-
-
-//     function  checkForSpam(message) {
-//       const lowerMessage = message.toLowerCase();
-           
-//       return lowerMessage.includes('sale')||lowerMessage.includes('spam')
-
-//       }
-  
-// console.log(checkForSpam("Latest technology news")); // false
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
-// console.log(checkForSpam("Get best sale offers now!")); // true
-// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-// console.log(checkForSpam("Trust me, this is not a spam message")); // true
-// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
 function filterArray(numbers, value) {
-    for 
+    let newArray = [];
 
+    for (let i = 0; i < numbers.length; i+=1) {
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]);
+        }
+    }
+
+    return newArray;
 }
-
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
